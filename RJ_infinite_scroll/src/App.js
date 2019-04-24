740229c4dfabf28321959aada09186856bd2f1ab
Rohit J <tihor29@pop-os.localdomain>
2019-04-24 18:38:18 -0400

    Fixed Scrolling issue

--- RJ_infinite_scroll/src/App.js
+++ RJ_infinite_scroll/src/App.js
@@ -2,6 +2,8 @@
     import logo from "./logo.svg";
     import "./App.css";
     import Image from "./Image.js"
+    //import { Text, View, StyleSheet, Dimensions, FlatList } from 'react-native';
+      var incrementor = 2258;
     class App extends Component {
       constructor(props) {
         super(props);
@@ -27,10 +29,13 @@
         //<div>
         //<img src={require('./busking/'+"IMG_"+incrementor+".jpg")}/>;
         //</div>);
-        var incrementor = 2258;
+
         for (var i = 0; i < this.state.items; i++) {
+          if (incrementor == 2262){
+            incrementor = 2258;
+          }
           items.push(
-            <Image src={require('./busking/'+"IMG_"+incrementor+".jpg")} height={500} width={500} mode='fit'/>
+            <Image src={require('./busking/'+"IMG_"+incrementor+".jpg")} height={700} width={500} mode='fit'/>
 
           );
          incrementor++;
@@ -39,11 +44,14 @@
       }
 
       loadMore() {
+        console.log(this.state.items);
         this.setState({ loading: true });
+
         setTimeout(() => {
           this.setState({ items: this.state.items + 5, loading: false });
         }, 2000);
       }
+
 
       render() {
         return (
@@ -64,6 +72,43 @@
           </div>
         );
       }
+
+//     var styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: '#ecf0f1',
+//   },
+//   image: {
+//     width: Dimensions.get('window').width,
+//     height: Dimensions.get('window').width,
+//   },
+//   info: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     padding: 10,
+//     paddingLeft: 15,
+//     paddingRight: 15,
+//     borderBottomWidth: 1,
+//     borderColor: '#d8d8d8',
+//   },
+//   infoText: {
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//   },
+//   comment: {
+//     flexDirection: 'row',
+//     padding: 10,
+//     paddingLeft: 15,
+//     borderBottomWidth: 1,
+//     borderColor: '#d8d8d8',
+//   },
+//   commentText: {
+//     paddingRight: 15,
+//     fontWeight: 'bold',
+//   },
+// })
     }
 
     export default App;
