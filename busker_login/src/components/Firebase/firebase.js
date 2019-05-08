2fddd2cf323946ca5cde96a1c579b52aa7a68994
rgj32 <rgj32@drexel.edu>
2019-05-08 13:19:49 -0400

    Firebase implementation and changed images

--- busker_login/src/components/Firebase/firebase.js
+++ busker_login/src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
-
+import firebase from "firebase"
 const config = {
   apiKey: "AIzaSyDLcrBLgAbWb3d_KE-DInA4IoL8svlsyvU",
   authDomain: "buskers-demo-vivek.firebaseapp.com",
@@ -29,6 +29,25 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
-}
 
+  addImages(){
+    const storageRef = firebase.storage().ref();
+    var mainImage;
+    var path = require('path')
+    var fs = require('fs')
+    const directoryPath = path.join('busking');
+    //passsing directoryPath and callback function
+    fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        mainImage = storageRef.child(file.name);
+    });
+});
+}
+}
 export default Firebase;
