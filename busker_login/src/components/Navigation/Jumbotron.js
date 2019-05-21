452e19ba892780c6e1277f9a84d5692c0039e0eb
Vivek Khimani <vck29@drexel.edu>
2019-05-21 00:01:51 -0400

    vivek latest changes

--- busker_login/src/components/Navigation/Jumbotron.js
+++ busker_login/src/components/Navigation/Jumbotron.js
@@ -1,21 +1,16 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import ReactDOM from "react-dom";
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 const HomeJumbotron = () => (
-<Container fluid>
-<Jumbotron>
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-</Jumbotron>
-</Container>
+  <Container fluid>
+    <Jumbotron>
+      <h1>CurbStyle</h1>
+      <p>We will be adding description soon! Thanks!</p>
+    </Jumbotron>
+  </Container>
 );
-
 
 export default HomeJumbotron;
--- busker_login/src/components/Navigation/index.js
+++ busker_login/src/components/Navigation/index.js
@@ -1,69 +1,86 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Image from 'react-bootstrap/Image';
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import ReactDOM from "react-dom";
-
 
 import * as ROUTES from "../../constants/routes";
 import SignOutButton from "../SignOut";
 
-
-
 const Navigation = () => (
-  <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-    <div class="container-fluid">
-      <a class="navbar-brand">CurbStyle </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
+  // <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+  //   <div class="container-fluid">
+  //     <a class="navbar-brand">CurbStyle </a>
+  //     <button
+  //       class="navbar-toggler"
+  //       type="button"
+  //       data-toggle="collapse"
+  //       data-target="#navbarResponsive"
+  //     >
+  //       <span class="navbar-toggler-icon" />
+  //     </button>
+  //
+  //     <div class="collapse navbar-collapse" id="navbarResponsive">
+  //       <ul class="navbar-nav ml-auto">
+  //         <li class="nav item">
+  //           <Link class="nav-link" to={ROUTES.ABOUT}>
+  //             About
+  //           </Link>
+  //         </li>
+  //         <li class="nav-item">
+  //           <Link class="nav-link" to={ROUTES.MAP}>
+  //             Map
+  //           </Link>
+  //         </li>
+  //         <li class="nav-item">
+  //           <Link class="nav-link" to={ROUTES.EVENTS}>
+  //             Events
+  //           </Link>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   </div>
+  //   <Form inline>
+  //   <InputGroup>
+  //     <InputGroup.Prepend>
+  //       <InputGroup.Text id="addon">@</InputGroup.Text>
+  //     </InputGroup.Prepend>
+  //     <FormControl
+  //       placeholder="Search"
+  //       aria-label="Username"
+  //       aria-describedby="addon"
+  //       />
+  //   </InputGroup>
+  //   </Form>
+  //   <Image style={{width: '40px', height: '40px'}} src="https://www.pinclipart.com/picdir/middle/133-1332476_crowd-of-users-transparent-user-icon-png-clipart.png" responsive/>
+  //   </nav>
 
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav item">
-            <Link class="nav-link" to={ROUTES.ABOUT}>
-              About
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link" to={ROUTES.MAP}>
-              Map
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link" to={ROUTES.EVENTS}>
-              Events
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <Form inline>
-    <InputGroup>
-      <InputGroup.Prepend>
-        <InputGroup.Text id="addon">@</InputGroup.Text>
-      </InputGroup.Prepend>
-      <FormControl
-        placeholder="Search"
-        aria-label="Username"
-        aria-describedby="addon"
-        />
-    </InputGroup>
-    </Form>
-    <Image style={{width: '40px', height: '40px'}} src="https://www.pinclipart.com/picdir/middle/133-1332476_crowd-of-users-transparent-user-icon-png-clipart.png" responsive/>
-    </nav>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">CurbStyle</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link to={ROUTES.HOME}>Home</Nav.Link>
+        <Nav.Link to={ROUTES.ABOUT}>About</Nav.Link>
+        <Nav.Link to={ROUTES.MAP}>Map</Nav.Link>
+        <Nav.Link to={ROUTES.EVENTS}>Events</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Username" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+      <Image
+        style={{ width: "40px", height: "40px" }}
+        src="https://www.pinclipart.com/picdir/middle/133-1332476_crowd-of-users-transparent-user-icon-png-clipart.png"
+        responsive
+      />
+    </Navbar.Collapse>
+  </Navbar>
 );
-
-
-
-
 
 export default Navigation;
